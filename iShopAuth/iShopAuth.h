b598f4b039f7850856b5f5fa10e58ce1b2a95c6e
Gustavo Adolfo Rodriguez <garodriguez263@gmail.com>
2024-05-04 21:59:14 -0500

    Firebase added

---
+++ iShopAuth/iShopAuth.h
@@ -0,0 +1,18 @@
+//
+//  iShopAuth.h
+//  iShopAuth
+//
+//  Created by Gustavo Adolfo Rodriguez on 4/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iShopAuth.
+FOUNDATION_EXPORT double iShopAuthVersionNumber;
+
+//! Project version string for iShopAuth.
+FOUNDATION_EXPORT const unsigned char iShopAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iShopAuth/PublicHeader.h>
+
+
