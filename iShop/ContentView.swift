9766a83903df065a8eb5d9b159cb4bfea8a52003
Gustavo Adolfo Rodriguez <garodriguez263@gmail.com>
2024-05-04 20:34:24 -0500

    Initial Commit

---
+++ iShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iShop
+//
+//  Created by Gustavo Adolfo Rodriguez on 4/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iShop/iShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShopApp.swift
+//  iShop
+//
+//  Created by Gustavo Adolfo Rodriguez on 4/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
